fa1f06e92264aba8766cc2b65e33748c2e453398
laurent.lin <laurent.lin@aswatson.com>
2023-06-29 17:27:14 +0800

    修改app.js

--- utils/api/upload/index.js
+++ utils/api/upload/index.js
@@ -44,45 +44,49 @@ function deleteFolderRecursive(path) {
 const upload = (isPro, app) => {
   // 处理文件上传的路由
   app.post('/upload', uploadInstance.single('file'), (req, res) => {
-    const file = req.file
-    console.log('上传文件信息', file)
+    try {
+      const file = req.file
+      console.log('上传文件信息', file)
 
-    const uploadFolder = isPro ? '/usr/share/nginx/html/node-project' : '../../../uploads/extracted'
-    const targetPath = path.join(__dirname, uploadFolder);
-    deleteFolderRecursive(targetPath)
+      const uploadFolder = isPro ? '/usr/share/nginx/html/node-project' : '../../../uploads/extracted'
+      const targetPath = path.join(__dirname, uploadFolder);
+      deleteFolderRecursive(targetPath)
 
-    // 获取上传的 zip 文件路径
-    const filePath = file.path;
+      // 获取上传的 zip 文件路径
+      const filePath = file.path;
 
-    // 使用 adm-zip 库解压文件
-    const zip = new AdmZip(filePath);
-    // const targetPath = '../../../uploads/extracted'; // 解压后的目标路径
-    zip.extractAllTo(targetPath, true);
-    const unlinkPath = path.join(__dirname, `../../../uploads/${file.filename}`)
-    fs.unlinkSync(unlinkPath)
+      // 使用 adm-zip 库解压文件
+      const zip = new AdmZip(filePath);
+      // const targetPath = '../../../uploads/extracted'; // 解压后的目标路径
+      zip.extractAllTo(targetPath, true);
+      const unlinkPath = path.join(__dirname, `../../../uploads/${file.filename}`)
+      fs.unlinkSync(unlinkPath)
 
-    console.log('测试zzzzisPro', isPro)
+      console.log('测试zzzzisPro', isPro)
 
-    if(isPro) {
-      console.log('zzzz')
-      const command = 'sudo systemctl restart nginx';
+      if(isPro) {
+        console.log('zzzz')
+        const command = 'sudo systemctl restart nginx';
 
-      exec(command, (error, stdout, stderr) => {
-        if (error) {
-          // 执行出错时的处理逻辑
-          console.error('执行命令出错:', error);
-          res.status(500).send('执行命令出错');
-          return;
-        }
-    
-        // 执行成功时的处理逻辑
-        console.log('命令执行结果:', stdout);
-        res.status(200).send('命令执行成功');
-      });
-    } else {
-      // 返回解压成功的响应
-      res.send('文件上传和解压成功');
-    }    
+        exec(command, (error, stdout, stderr) => {
+          if (error) {
+            // 执行出错时的处理逻辑
+            console.error('执行命令出错:', error);
+            res.status(500).send('执行命令出错');
+            return;
+          }
+      
+          // 执行成功时的处理逻辑
+          console.log('命令执行结果:', stdout);
+          res.status(200).send('命令执行成功');
+        });
+      } else {
+        // 返回解压成功的响应
+        res.send('文件上传和解压成功');
+      }
+    } catch (err) {
+      console.log('测试uploadErr', err)
+    }        
   });
 
   // app.get('/yyy', (req, res) => {
